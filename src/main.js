d57ee0dcc134f03ec53b33275f4787d67b1fb7be
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-05 20:19:20 +0100

    Adds vue-fontawesome config in main application files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue';
 import { BootstrapVue } from 'bootstrap-vue';
 import VueToastr from 'vue-toastr';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+library.add(faGithub);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(BootstrapVue);
 
